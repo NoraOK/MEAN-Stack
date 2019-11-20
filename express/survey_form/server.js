43569dbf6aac8e31d2aabf78dd3635f593304ad4
NoraOK <n.okeeffe35@gmail.com>
2019-11-19 16:28:50 -0800

    added socket

--- express/survey_form/server.js
+++ express/survey_form/server.js
@@ -4,21 +4,36 @@ app.use(express.static(__dirname + '/static'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname +'/views');
 app.use(express.urlencoded({extended:true}));
+const server = app.listen(8000, () => console.log('listening on port 8000 <3'));
+const io = require('socket.io')(server);
+var counter = 0;
+    
 
 app.get('/', (req,res) =>{
     res.render('index')
 });
 
-app.post('/results', (req,res)=>{
-    console.log('Got the post form!')
-    var name = req.body.name;
-    var location = req.body.location;
-    var language = req.body.language;
-    var comment = req.body.comment;
+io.on('connection', function (socket) { //2
+    console.log('connection is running');
+    socket.on('posting_form', function(msg){
+      console.log('test');
+      console.log('posting_form: ' + msg.name+", "+msg.location); //8 (note: this log will be on your server's terminal)
+      msg.randnum = Math.floor((Math.random()*999) + 1);
+      console.log(msg.randnum)
+      socket.emit('updated_message', msg)
+    });
 
-    res.render('results',{name: name, location: location, language: language, comment: comment})
 });
 
+// app.post('/results', (req,res)=>{
+//     console.log('Got the post form!')
+//     var name = req.body.name;
+//     var location = req.body.location;
+//     var language = req.body.language;
+//     var comment = req.body.comment;
+
+//     res.render('results',{name: name, location: location, language: language, comment: comment})
+// });
 
 
-app.listen(8000, () => console.log('listening on port 8000 <3'));
+
